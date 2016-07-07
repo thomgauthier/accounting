ce3a6c2eefa31a30e9778cf401cf67a678035924
thomas <tgr.gauthier@gmail.com>
2016-07-07 22:22:57 +0200

    -

--- tgr-web-client/app/app.component.ts
+++ tgr-web-client/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { MenuComponent } from 'app/component/menu/menu.component.ts';
+//import { MenuComponent } from 'app/component/menu/menu.component.ts';
 @Component({
     selector: 'accounting-app',
     templateUrl: 'app/application.html',
-    directives: [MenuComponent]
+    //    directives: [MenuComponent]
 })
 export class AppComponent { }
